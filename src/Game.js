b3e86ee63648460e04ee1ebaf6615c33fc9d9b9a
David Jordan <wordnerd92@gmail.com>
2023-04-01 16:55:35 -0500

    working tictactoe game

---
+++ src/Game.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import Board from './Board';
+import './Game.css';
+
+function Game() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    const squaresCopy = [...squares];
+    if (calculateWinner(squaresCopy) || squaresCopy[i]) {
+      return;
+    }
+    squaresCopy[i] = xIsNext ? 'X' : 'O';
+    setSquares(squaresCopy);
+    setXIsNext(!xIsNext);
+  }
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  };
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
+  } else if (squares.every((square) => square !== null)) {
+    status = 'Draw!';
+  } else {
+    status = `It's player ${xIsNext ? 'X' : 'O'}'s turn`;
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={squares} onClick={handleClick} />
+      </div>
+      <div className="game-info">
+        <div className="status">{status}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Game;
